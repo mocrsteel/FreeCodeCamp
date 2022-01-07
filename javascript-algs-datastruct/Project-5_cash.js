6bf04bf70c81f72a2c2274ab1d91124fa955da13
Yannick Thimmesch <yannick@MacBook-Air-van-Yannick.local>
2022-01-07 23:13:26 +0100

    JS Project 5 updated a bit.

--- javascript-algs-datastruct/Project-5_cash.js
+++ javascript-algs-datastruct/Project-5_cash.js
@@ -1,24 +1,23 @@
 function checkCashRegister(price, cash, cid) {
     let changeRequired = cash - price;
-    let totalFunds = Object.values(cid).reduce((sum, arr) => sum += arr[1], 0) >= changeRequired;
-    if (sufficientFunds) {
-        return "it's possible"
-    } else {
+    let totalFunds = Object.values(cid).reduce((sum, arr) => sum += arr[1], 0);
+    if (totalFunds < changeRequired) {
         return {
             status: "INSUFFICIENT_FUNDS",
             change: [],
+        };
+    } else if (totalFunds === changeRequired){
+        return {
+            status: "CLOSED",
+            change: cid,
+        };
+    } else {
+        console.log(changeRequired)
+        for (let i = cid.length-1; i >= 0; i--) {
+            console.log(cid[i]);
         }
+        return "enough cash";
     }
-    let change, status;
-    /*
-        INSUFFICIENT_FUNDS = less than change cash in drawer (cid)
-        CLOSED = cid === change,
-        OPEN = cid > change and change can be given correclty,
-    */
-    return {
-        status: status,
-        change: change,
-    };
   }
   
-  checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+  console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
