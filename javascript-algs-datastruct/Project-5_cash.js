230d1aad64b1602637b7293c7140b7391fa4d220
mocrsteel <yannick.thimmesch@gmail.com>
2022-01-08 21:00:31 +0100

    some more work

--- javascript-algs-datastruct/Project-5_cash.js
+++ javascript-algs-datastruct/Project-5_cash.js
@@ -1,6 +1,19 @@
 function checkCashRegister(price, cash, cid) {
+    const moneyNames = {
+        "ONE HUNDRED": 100,
+        "TWENTY": 20,
+        "TEN": 10,
+        "FIVE": 5,
+        "ONE": 1,
+        "QUARTER": 0.25,
+        "DIME": 0.1,
+        "NICKEL": 0.05,
+        "PENNY": 0.01,
+    };
     let changeRequired = cash - price;
-    let totalFunds = Object.values(cid).reduce((sum, arr) => sum += arr[1], 0);
+    const totalFunds = Object.values(cid).reduce((sum, arr) => sum += arr[1], 0);
+    let sortedCID = cid.sort((a, b) => moneyNames[b[0]] - moneyNames[a[0]]);
+    
     if (totalFunds < changeRequired) {
         return {
             status: "INSUFFICIENT_FUNDS",
@@ -12,12 +25,20 @@ function checkCashRegister(price, cash, cid) {
             change: cid,
         };
     } else {
+        let returnedCash = {};
+        for (let i = 0; i <= sortedCID.length - 1; i++) {
+            let [key, amount] = sortedCID[i];
+            let numUnits = Math.floor(changeRequired / moneyNames[key]);
+            if (changeRequired > 0 && numUnits > 0) {
+                console.log(changeRequired, numUnits, numUnits * moneyNames[key])
+                returnedCash[key] = Math.round(numUnits * moneyNames[key] * 100) / 100;
+                changeRequired -= numUnits * moneyNames[key];
+            };
+        };
         console.log(changeRequired)
-        for (let i = cid.length-1; i >= 0; i--) {
-            console.log(cid[i]);
-        }
-        return "enough cash";
+        return returnedCash;
     }
   }
   
   console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+  console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]))
