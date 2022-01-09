ad5058f34768d382ad97d2904624cb0476c49555
mocrsteel <yannick.thimmesch@gmail.com>
2022-01-09 17:05:04 +0100

    Completed JS basic course

--- javascript-algs-datastruct/Project-5_cash.js
+++ javascript-algs-datastruct/Project-5_cash.js
@@ -12,14 +12,9 @@ function checkCashRegister(price, cash, cid) {
     };
     let changeRequired = cash - price;
     const totalFunds = Object.values(cid).reduce((sum, arr) => sum += arr[1], 0);
-    let sortedCID = cid.sort((a, b) => moneyNames[b[0]] - moneyNames[a[0]]);
+    let sortedCID = cid.concat().sort((a, b) => moneyNames[b[0]] - moneyNames[a[0]]);
     
-    if (totalFunds < changeRequired) {
-        return {
-            status: "INSUFFICIENT_FUNDS",
-            change: [],
-        };
-    } else if (totalFunds === changeRequired){
+    if (totalFunds === changeRequired){
         return {
             status: "CLOSED",
             change: cid,
@@ -36,10 +31,20 @@ function checkCashRegister(price, cash, cid) {
                     (changeRequired - returnedAmount) * 100) / 100;
             };
         };
-        console.log(changeRequired)
-        return {status: "OPEN", change: returnedCash};
+        return returnedCash.reduce((sum, subArr) => sum += subArr[1], 0) < changeRequired
+                    ? {status: "INSUFFICIENT_FUNDS", change: []}
+                    : {status: "OPEN", change: returnedCash} 
+
     }
-  }
+}
   
-  console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
-  console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]))
+  
+console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
+console.log(checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
+console.log(checkCashRegister(
+    19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0],
+    ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]))
+
+console.log(checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]))
