1deeb40d42e2d82893f49743991913164a06ed0f
mocrsteel <yannick.thimmesch@gmail.com>
2022-01-11 23:02:29 +0100

    first frontend project

---
+++ frontend-libs/random-quote-generator/src/App.js
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App () {
+  return (
+    <div id="quote-box" className="App">
+      <div id="text">
+
+      </div>
+      <div id="author">
+
+      </div>
+    </div>
+  )
+}
+
+export default App
