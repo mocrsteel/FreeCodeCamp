a63a0b7f60ef55c69d6042c197bd135dfda5ffde
mocrsteel <yannick.thimmesch@gmail.com>
2022-01-16 21:25:43 +0100

    updated css

--- frontend-libs/ts-quote-generator/src/App.tsx
+++ frontend-libs/ts-quote-generator/src/App.tsx
@@ -5,7 +5,11 @@ const QuoteContent = () => {
   return (
     <div id="quote-content">
       <div id="text" className='card-text p-5'>
-      Testing element here.
+      <p>
+        <i className="bi bi-quote"/>
+        Testing element here
+        <i className="bi bi-quote bi-quote-flip" />
+      </p>
       </div>
       <div id="author" ></div>
     </div>
@@ -14,21 +18,40 @@ const QuoteContent = () => {
 
 const QuoteButtons = () => {
   return (
-    <div id="buttons" className="w-100 d-flex flex-row justify-content-between m-5">
-        <div id="socials" className="">
+    <div id="buttons" className="d-flex flex-row justify-content-between my-3 mx-4">
+        <div id="socials">
           <button
-            id="tweet-quote"
-            className="btn btn-primary btn-circle btn-sm card-link" type="button">
-            <i className='bi bi-twitter'/></button>
+            className="btn btn-circle me-1">
+            <a
+              id="tweet-quote"
+              href="https://twitter.com/intent/tweet"
+              target="_blank"
+              rel="noreferrer">
+                <i className='bi bi-twitter'/>
+              </a></button>
+          <button className='btn btn-circle me-1'>
+            <a
+              id="facebook-quote"
+              href="https://www.facebook.com/"
+              target="_blank" rel="noreferrer">
+                <i className='bi bi-facebook' />
+              </a>
+          </button>
+          <button className="btn btn-circle">
+            <a
+              id="whatsapp-quote"
+              href="https://web.whatsapp.com/"
+              target="_blank" rel="noreferrer">
+                <i className="bi bi-whatsapp" />
+              </a>
+          </button>
           </div>
         <div>
-          <button id="new-quote" className="btn btn-sm card-link">New Quote</button>
+          <button id="new-quote" className="btn btn-bullet">New Quote</button>
         </div>
     </div>
   )
 }
-
-// left at arranging the buttons as I want them.
 
 const QuoteBox = () => {
   return (
@@ -41,7 +64,7 @@ const QuoteBox = () => {
 
 const App = () => {
   return (
-  <div className="h-100 d-flex justify-content-center">
+  <div className="container-fluid h-100 d-flex justify-content-center">
     <QuoteBox />
   </div>
   )
