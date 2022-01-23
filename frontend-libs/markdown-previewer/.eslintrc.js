34d2cda44cd18267cccd2ab29451527c1169bede
mocrsteel <yannick.thimmesch@gmail.com>
2022-01-23 12:16:49 +0100

    Cleaned eslintrc and base files

--- frontend-libs/markdown-previewer/.eslintrc.js
+++ frontend-libs/markdown-previewer/.eslintrc.js
@@ -1,28 +1,30 @@
 module.exports = {
-    root: true,
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    'jest/globals': true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    "extends": [
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended",
-        "standard"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint",
-        "jest"
-    ],
-    "rules": {
-    }
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'jest'
+  ],
+  rules: {
+    'no-use-before-define': 'off'
+  }
 }
