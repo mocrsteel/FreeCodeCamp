d0816199161fb083923e161e86130e6be9cde448
mocrsteel <yannick.thimmesch@gmail.com>
2022-02-02 18:43:55 +0100

    :white_check_mark: Finished markdown previewer

--- frontend-libs/markdown-previewer/src/App.tsx
+++ frontend-libs/markdown-previewer/src/App.tsx
@@ -2,12 +2,12 @@ import * as React from 'react'
 import { Fragment, useState } from 'react'
 import './styles/App.scss'
 import { marked } from 'marked'
+import hljs from 'highlight.js'
 
 export type mdProps = {
   value: string,
   handleChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void
 }
-
 export type defaultEditorContent = string
 
 export const Editor = ({ value, handleChange }: mdProps): JSX.Element => {
@@ -23,6 +23,16 @@ export const Editor = ({ value, handleChange }: mdProps): JSX.Element => {
 
 export const Preview = ({ value }: { value: mdProps['value'] }): JSX.Element => {
   // function gives errors for now. Check console log and figure out what to to.
+  marked.setOptions({
+    renderer: new marked.Renderer(),
+    highlight: (code, lang) => {
+      const language = hljs.getLanguage(lang) ? lang : 'plaintext'
+      return hljs.highlight(code, { language }).value
+    },
+    langPrefix: 'hljs language-',
+    breaks: true,
+    smartLists: true
+  })
   return (
     <Fragment>
       <div className='Previewer'>
@@ -38,7 +48,10 @@ const App = () => {
   const defaultEditorContent = '# Main Title\n# Sub Title 1\n## Sub Title 2' +
     '\n\nLook at [Duck Duck Go](https://duckduckgo.com)' +
     '\nSome options for a `code` block:\n* Python\n* JavaScript\n* SASS' +
-    '\n\n```\nfor (let i = 0; i < 25; i++) {\n\tconsole.log(hello world!)\n}\n```'
+    '\n\n```\nfor (let i = 0; i < 25; i++) {\n\tconsole.log(hello world!)\n}\n```' +
+    '\nAnd not to forget: a **block quote** below this line.' +
+    '\n> A random quote - Random Guy' +
+    '\n\n\n\n![MacOS](https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/MacOS_logo_%282017%29.svg/512px-MacOS_logo_%282017%29.svg.png)'
   const [textAreaValue, updateTextAreaValue] = useState(defaultEditorContent)
   const handleTextAreaChange = (e: React.ChangeEvent<HTMLTextAreaElement>): void => {
     updateTextAreaValue(e.currentTarget.value)
