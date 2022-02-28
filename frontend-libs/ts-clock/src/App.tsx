0f8608ba5e5fb148e75d1f5f91526a998263b4a7
mocrsteel <yannick.thimmesch@gmail.com>
2022-02-28 20:31:06 +0100

    :recycle: ts-clock WIP

--- frontend-libs/ts-clock/src/App.tsx
+++ frontend-libs/ts-clock/src/App.tsx
@@ -2,19 +2,40 @@ import React from 'react'
 import './App.sass'
 
 function App () {
-  const [sessionLength, updateSessionLength] = React.useState([5, 0])
-  const [breakLength, updateBreakLength] = React.useState([25, 0])
-  
+  const [sessionLength, updateSessionLength] = React.useState(5)
+  const [breakLength, updateBreakLength] = React.useState(25)
+  // const [isRunning, updateIsRunning] = React.useState(true)
+
+  const clickHandler = (e: React.MouseEvent) => {
+    console.log(e.currentTarget.id)
+    switch (e.currentTarget.id) {
+      case 'break-increment':
+        updateBreakLength(breakLength + 1)
+        break
+      case 'break-decrement':
+        updateBreakLength(breakLength - 1)
+        break
+      case 'session-increment':
+        updateSessionLength(sessionLength + 1)
+        break
+      case 'session-decrement':
+        updateSessionLength(sessionLength - 1)
+        break
+      default:
+        console.warn(`Unidentified case ${e.currentTarget.id}`)
+        break
+    }
+  }
   return (
     <div className="App">
       <h1 id='break-label'>Break Length</h1>
       <h2 id="break-length">{breakLength}</h2>
-      <button id='break-increment'></button>
-      <button id='break-decrement'></button>
+      <button id='break-increment' onClick={clickHandler}> + </button>
+      <button id='break-decrement' onClick={clickHandler}> - </button>
       <h1 id="session-label">Session Length</h1>
       <h2 id="session-length">{sessionLength}</h2>
-      <button id='session-increment'></button>
-      <button id='session-decrement'></button>
+      <button id='session-increment' onClick={clickHandler}> + </button>
+      <button id='session-decrement' onClick={clickHandler}> - </button>
     </div>
   )
 }
